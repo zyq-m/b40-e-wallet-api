f719b78195fbaa5adf1e7ffeddfd6d115d812392
Haziq Musa <last.game01987@gmail.com>
2022-08-15 16:57:36 +0800

    ejas sikit file query utk deploy ke heroku

--- routes/query.js
+++ routes/query.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const Pool = require("pg").Pool;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
 const pool = new Pool({
-  user: "me",
-  host: "localhost",
-  database: "b40api",
-  password: "password",
-  port: 5432,
+  // connect db to application
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  // comment out ssl code if run in development
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 module.exports = pool;

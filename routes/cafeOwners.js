5837cb44fa5a0514c7ef86f780dae8a5eef13735
Haziq Musa <last.game01987@gmail.com>
2022-11-16 13:18:15 +0800

    refactor query

--- routes/cafeOwners.js
+++ routes/cafeOwners.js
@@ -3,20 +3,23 @@ const router = express.Router();
 const pool = require("./query");
 
 const getCafe = (request, response) => {
-  pool.query("SELECT * FROM cafe_owners", (error, results) => {
-    if (error) return response.status(500);
-    return response.status(200).json(results.rows);
-  });
+  pool.query(
+    "SELECT username, owner_name, active, cafe_name FROM cafe_owners",
+    (error, results) => {
+      if (error) return response.status(500);
+      return response.status(200).json(results.rows);
+    }
+  );
 };
 
 const getCafeById = (request, response) => {
   const id = request.params.id;
 
   pool.query(
-    "SELECT * FROM cafe_owners WHERE username = $1",
+    "SELECT username, owner_name, active, cafe_name FROM cafe_owners WHERE username = $1",
     [id],
     (error, results) => {
-      if (error) return response.status(500);
+      if (error) return response.sendStatus(500);
       if (results.rowCount === 0) return response.sendStatus(404);
       return response.status(200).json(results.rows);
     }
@@ -30,11 +33,14 @@ const createCafe = (request, response) => {
     "INSERT INTO cafe_owners (username, password, owner_name, cafe_name) VALUES ($1, $2, $3, $4) RETURNING *",
     [username, password, owner_name, cafe_name],
     (error, results) => {
-      if (error)
-        return response.status(500).send({ message: "Cafe already exist" });
-      return response
-        .status(201)
-        .send(`User added with ID: ${results.rows[0].username}`);
+      if (error) {
+        if (error.code === "23505") {
+          return response.send("user exist").status(500);
+        }
+        return response.sendStatus(500);
+      }
+
+      return response.sendStatus(201);
     }
   );
 };

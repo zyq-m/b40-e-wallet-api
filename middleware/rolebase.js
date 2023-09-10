8f26e339d557632a2bafd7375b93963de7fa5b4a
Haziq Musa <last.game01987@gmail.com>
2023-09-11 01:18:46 +0800

    refactor rolebase middleware

--- middleware/rolebase.js
+++ middleware/rolebase.js
@@ -1,28 +1,13 @@
-const studentRole = (req, res, next) => {
-  if (req?.user?.role == "student") {
-    next();
-    return;
-  }
+const roleMiddleware = roles => {
+  return (req, res, next) => {
+    const userRole = req.user.role;
 
-  res.status(400).send({ message: "You cannot access this route" });
+    if (!roles.includes(userRole)) {
+      return res.status(400).send({ message: "You cannot access this route" });
+    }
+
+    next();
+  };
 };
 
-const cafeRole = (req, res, next) => {
-  if (req?.user?.role == "cafe") {
-    next();
-    return;
-  }
-
-  res.status(400).send({ message: "You cannot access this route" });
-};
-
-const adminRole = (req, res, next) => {
-  if (req?.user?.role == "admin") {
-    next();
-    return;
-  }
-
-  res.status(400).send({ message: "You cannot access this route" });
-};
-
-module.exports = { studentRole, cafeRole, adminRole };
+module.exports = roleMiddleware;
